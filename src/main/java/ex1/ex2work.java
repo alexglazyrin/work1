8d90221a3c734b57f0d4953bda3271be075f9499
A.Glazyrin <alex_glazyrin@yahoo.com>
2021-12-23 14:49:40 +0300

    2nd commit

--- src/main/java/ex1/ex2work.java
+++ src/main/java/ex1/ex2work.java
@@ -2,10 +2,13 @@ package ex1;
 
 public class ex2work {
     public static void main(String[] args) {
+        counter(4,4);
+    }
+    public static void counter (int s, int c){
         int counter = 1;
-        int[][] table = new int[3][4];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 4; j++) {
+        int[][] table = new int[s][c];
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j < c; j++) {
                 table[i][j] = counter;
                 System.out.print(table[i][j] + " ");
                 counter++;
