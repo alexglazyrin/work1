9117d47293070b6c8ce6ba92511f3214d48a844f
A.Glazyrin <alex_glazyrin@yahoo.com>
2021-12-23 15:26:00 +0300

    3rd commit

--- src/main/java/ex1/ex2work.java
+++ src/main/java/ex1/ex2work.java
@@ -1,8 +1,14 @@
 package ex1;
 
+import java.util.Scanner;
+
 public class ex2work {
     public static void main(String[] args) {
-        counter(4,4);
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+        counter(s,c);
     }
     public static void counter (int s, int c){
         int counter = 1;
@@ -12,6 +18,14 @@ public class ex2work {
                 table[i][j] = counter;
                 System.out.print(table[i][j] + " ");
                 counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void printArr (int [][] arr){
+        for (int i=0; i< arr.length; i++){
+            for (int j=0; j<arr[i].length;j++){
+                System.out.println(arr[i][j]);
             }
             System.out.println();
         }
